e618c1908784ee3c843e6ab435e5581de5b1ef38
Dursun Köktürk <dursunserefkokturk@gmail.com>
2026-06-01 11:51:18 +0300

    src klasoru icindeki app.js component icine mobile-background eklendi, app.css dosyasinda css duzenlemesi yapildi

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import MobileBackground from './assets/img/mobile-background.png'
 import './App.css'
 
 export default function App() {
 
   return (
     <>
+    <img src={MobileBackground} alt="" />
     </>
   )
 }
